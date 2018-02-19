13b60fa7733e1c5912d77ce7f5131b547c0d65f6
hammeradam <hammeradam894@gmail.com>
2018-02-19 12:36:46 +0100

    :lipstick: Updated UI

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-var elem = document.querySelector(".side-nav");
-var instance = M.Sidenav.init(elem, options);
+(function ($) {
+    $(function () {
+
+        //initialize all modals           
+        $('.modal').modal();
+
+        $(".button-collapse").sideNav();
+
+        //now you can open modal from code
+        $('#modal2').modal('open');
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
