541636e35f260c8b5ead552ba66416f701698d62
hammeradam <hammeradam894@gmail.com>
2018-02-19 13:45:39 +0100

    :lipstick: Updated UI

--- index.js
+++ index.js
@@ -6,8 +6,10 @@
 
         $(".button-collapse").sideNav();
 
+        $('.chips').material_chip();
+
         //now you can open modal from code
-        $('#modal2').modal('open');
+        // $('#modal2').modal('open');
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
